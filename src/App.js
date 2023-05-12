94537343f3351c9092514cc5d79f78b3fd914f61
Sydney McGee <mcgee.sydney@gmail.com>
2023-05-11 22:12:54 -0500

    Completed Entry Chart Tracker Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 275, date: '11-23-2021'}, {weight: 270, date: '11-24-2021'}])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries)
+  }
+
 
   return (
     <div>
       <DisplayEntries parentEntries = {entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <EntriesChartTracker parentEntries = {entries} />
 
     </div>
   );
